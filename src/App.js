27a7540aa3f09795186d0abda9dd3fd2735ea6f0
Vijay <vijaysadhuram4868@gmail.com>
2023-06-13 22:51:48 +0530

    injecting {} braces

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 function App() {
   return (
     <div className="header">
+      phone directory
+      <div>
+        <button>ADD</button>
+        <div>
+        <label>Username: </label>
+        <input placeholder="Enter Name"></input> <br/>
+        <label>Phone: </label>
+        <input placeholder="Enter Phone" defaultValue={a+b}></input>
+        </div>
       
     </div>
   );
